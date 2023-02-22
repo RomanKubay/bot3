23efbc01fb2542514eed5066b4718f60ebabfc8d
RomanKubay <romankubaj40@gmail.com>
2023-02-23 01:00:21 +0200

    modified:   .gitignore 	modified:   config.py

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
-﻿# import os
-# API_KEY = os.environ['API_KEY']
-# MONGODB_HOST = os.environ['MONGODB_HOST']
-
-API_KEY = '6206166136:AAG4JbHPHz0Fcx15zZdeVLZH9xFtm8DKpuU'
-MONGODB_HOST = 'mongodb+srv://RomanKubay:7iunrPEfsDm8UAS8@cluster.myvigzq.mongodb.net/?retryWrites=true&w=majority'
+﻿import os
+API_KEY = os.environ['API_KEY']
+MONGODB_HOST = os.environ['MONGODB_HOST']
